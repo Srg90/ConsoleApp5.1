99c13061fe43e629903f803d19ab3878c8c2fccf
Srg90 <94631413+Srg90@users.noreply.github.com>
2021-11-30 11:17:24 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -14,29 +14,27 @@ namespace ConsoleApp5._1
             Console.WriteLine("Поочередно введите семь любых чисел");
             Console.WriteLine();
             int n = 0;
-            int sum = 0;
-              
-            number[0] = Convert.ToInt32(Console.ReadLine());
-            number[1] = Convert.ToInt32(Console.ReadLine());
-            number[2] = Convert.ToInt32(Console.ReadLine());
-            number[3] = Convert.ToInt32(Console.ReadLine()); 
-            number[4] = Convert.ToInt32(Console.ReadLine());
-            number[5] = Convert.ToInt32(Console.ReadLine());
-            number[6] = Convert.ToInt32(Console.ReadLine());
+            double sum = 0;
+            
             for (int i = 0; i < 7; i++)
             {
-                //number[i] = Convert.ToInt32(Console.ReadKey());
+                number[i] = Convert.ToInt32(Console.ReadLine());
+            }  
+            
+            for (int j = 0; j < 7; j++)
+            {
+                
                 n += 1;
-                sum += number[i];
+                sum += number[j];
                 Console.WriteLine();
-                Console.WriteLine("Число {0} = {1}", n, number[i]);
-                
-                
+                Console.WriteLine("Число {0} = {1}", n, number[j]);
+                              
             }
+            
             Console.WriteLine();
             Console.WriteLine("Сумма введеных чисел = {0}", sum);
             Console.WriteLine();
-            Console.WriteLine("Среднее арифмитическое значение = {0}", sum / n);
+            Console.WriteLine("Среднее арифмитическое значение = {0}", Math.Round((sum / n), 1));
             Console.ReadKey();
         }
 
